77a5a906d5117f3bbd3b2ed480dc1f4e81edbb62
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-05-23 14:22:32 +0530

    Cyrus

---
+++ NekoRobot/hacking_script.py
@@ -0,0 +1,18 @@
+PM_START_TEXT = """
+────「 [{}] 」────
+*やあ! Hello {} My Darling ,
+I'm An Anime Themed Multi-Featured & Advanced Group Management Bot To Manage Your Group.*
+
+◈ *Version :* `3.10.2`
+◈ *Server Uptime:* `{}`
+◈ `{}` *users, across* `{}` *chats.*
+
+➢ Try The /help Buttons To See The Commands Available After My Element Buff ××
+"""
+
+
+PM_START_PIC = "https://te.legra.ph/file/9abdbb8e7fa75e03a91ea.jpg"
+
+
+## Don't Remove That Credits in PM_START_TEXT if yoy removed your app will be crashed.
+
