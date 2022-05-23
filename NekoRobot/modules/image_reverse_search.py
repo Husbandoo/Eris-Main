6c72e58301cc41f570916a22aa326fe84cec61d7
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-05-23 14:55:27 +0530

    Cyrus

--- NekoRobot/modules/image_reverse_search.py
+++ NekoRobot/modules/image_reverse_search.py
@@ -212,7 +212,7 @@ __help__ = f"""
 __mod_name__ = "Reverse"
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
-    "reverse", reverse, pass_args=True, admin_ok=True
+    ["reverse","grs","pp","pt"], reverse, pass_args=True, admin_ok=True, run_async=True
 )
 
 dispatcher.add_handler(REVERSE_HANDLER)
