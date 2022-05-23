77dd85e002bb239a92be1fc7eb297ede3913fda1
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-05-23 14:38:00 +0530

    Cyrus

--- NekoRobot/chatbot_sql.py
+++ NekoRobot/chatbot_sql.py
@@ -21,18 +21,47 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from NekoRobot import DB_URI
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+import threading
+
+from sqlalchemy import Column, String
+from NekoRobot.modules.sql import BASE, SESSION
+
+class KukiChats(BASE):
+    __tablename__ = "kuki_chats"
+    chat_id = Column(String(14), primary_key=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+KukiChats.__table__.create(checkfirst=True)
+INSERTION_LOCK = threading.RLock()
 
 
-def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+def is_kuki(chat_id):
+    try:
+        chat = SESSION.query(KukiChats).get(str(chat_id))
+        return bool(chat)
+    finally:
+        SESSION.close()
+
+def set_kuki(chat_id):
+    with INSERTION_LOCK:
+        kukichat = SESSION.query(KukiChats).get(str(chat_id))
+        if not kukichat:
+            kukichat = KukiChats(str(chat_id))
+        SESSION.add(kukichat)
+        SESSION.commit()
+
+def rem_kuki(chat_id):
+    with INSERTION_LOCK:
+        kukichat = SESSION.query(KukiChats).get(str(chat_id))
+        if kukichat:
+            SESSION.delete(kukichat)
+        SESSION.commit()
 
 
-BASE = declarative_base()
-SESSION = start()
+def get_all_kuki_chats():
+    try:
+        return SESSION.query(KukiChats.chat_id).all()
+    finally:
+        SESSION.close()
