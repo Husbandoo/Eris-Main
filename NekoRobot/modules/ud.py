51a646b6101f79e75f00410548f6aede8e725bec
Xelcius <97951640+RimuruDemonlord@users.noreply.github.com>
2022-05-25 00:41:48 -0700

    Update ud.py

--- NekoRobot/modules/ud.py
+++ NekoRobot/modules/ud.py
@@ -1,25 +1,31 @@
-from telegram import Update, Bot
-from telegram.ext import run_async
-
-from NekoRobot.modules.disable import DisableAbleCommandHandler
+import requests
 from NekoRobot import dispatcher
+from NekoRobot.modules.disable import DisableAbleCommandHandler
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
 
-from requests import get
 
 @run_async
-def ud(bot: Bot, update: Update):
-  message = update.effective_message
-  text = message.text[len('/ud '):]
-  results = get(f'http://api.urbandictionary.com/v0/define?term={text}').json()
-  reply_text = f'Word: {text}\nDefinition: {results["list"][0]["definition"]}'
-  message.reply_text(reply_text)
+def ud(update: Update, context: CallbackContext):
+    message = update.effective_message
+    text = message.text[len("/ud ") :]
+    results = requests.get(
+        f"https://api.urbandictionary.com/v0/define?term={text}"
+    ).json()
+    try:
+        reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+    except:
+        reply_text = "No results found."
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+
+
+UD_HANDLER = DisableAbleCommandHandler(["ud"], ud)
+
+dispatcher.add_handler(UD_HANDLER)
 
 __help__ = """
- 🔹 `/ud:{word}` Type the word or expression you want to search use. like `/ud` telegram Word: Telegram Definition: A once-popular system of telecommunications, in which the sender would contact the telegram service and speak their [message] over the [phone]. The person taking the message would then send it, via a teletype machine, to a telegram office near the receiver's [address]. The message would then be hand-delivered to the addressee. From 1851 until it discontinued the service in 2006, Western Union was the best-known telegram service in the world.
+» /ud (text) *:* Searchs the given text on Urban Dictionary and sends you the information.
 """
-
-__mod_name__ = "Urban dictionary"
-  
-ud_handle = DisableAbleCommandHandler("ud", ud)
-
-dispatcher.add_handler(ud_handle)
+__mod_name__ = "Uʀʙᴀɴ D"
+__command_list__ = ["ud"]
+__handlers__ = [UD_HANDLER]
