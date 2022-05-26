76186582a7e35ca62aa223e07654fcf4b7db2f10
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2022-05-26 22:13:17 +0530

    Update misc.py

--- NekoRobot/modules/helper_funcs/misc.py
+++ NekoRobot/modules/helper_funcs/misc.py
@@ -4,16 +4,12 @@ from typing import Dict, List, Union
 from html import escape
 import ffmpeg
 
-from math import ceil
-
-
 from NekoRobot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
-
     def __eq__(self, other):
         return self.text == other.text
 
@@ -28,79 +24,66 @@ def split_message(msg: str) -> List[str]:
     if len(msg) < MAX_MESSAGE_LENGTH:
         return [msg]
 
+    lines = msg.splitlines(True)
+    small_msg = ""
+    result = []
+    for line in lines:
+        if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
+            small_msg += line
+        else:
+            result.append(small_msg)
+            small_msg = line
     else:
-        lines = msg.splitlines(True)
-        small_msg = ""
-        result = []
-        for line in lines:
-            if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
-                small_msg += line
-            else:
-                result.append(small_msg)
-                small_msg = line
         # Else statement at the end of the for loop, so append the leftover string.
         result.append(small_msg)
 
-        return result
+    return result
 
 
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
-        modules = sorted([
-            EqInlineKeyboardButton(
-                x.__mod_name__,
-                callback_data="{}_module({})".format(prefix,
-                                                     x.__mod_name__.lower()))
-            for x in module_dict.values()
-        ])
+        modules = sorted(
+            [
+                EqInlineKeyboardButton(
+                    x.__mod_name__,
+                    callback_data="{}_module({})".format(
+                        prefix, x.__mod_name__.lower(),
+                    ),
+                )
+                for x in module_dict.values()
+            ],
+        )
     else:
-        modules = sorted([
-            EqInlineKeyboardButton(
-                x.__mod_name__,
-                callback_data="{}_module({},{})".format(prefix, chat,
-                                                        x.__mod_name__.lower()))
-            for x in module_dict.values()
-        ])
+        modules = sorted(
+            [
+                EqInlineKeyboardButton(
+                    x.__mod_name__,
+                    callback_data="{}_module({},{})".format(
+                        prefix, chat, x.__mod_name__.lower(),
+                    ),
+                )
+                for x in module_dict.values()
+            ],
+        )
 
-    pairs = [
-    modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
-    ]
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
 
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
-    if calc == 1:
-        pairs.append((modules[-1], ))
-    elif calc == 2:
-        pairs.append((modules[-1], ))
-
-    max_num_pages = ceil(len(pairs) / 10)
-    modulo_page = page_n % max_num_pages
-
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 8:
-        pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("◁", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Nexus", url="https://t.me/NexusXSupport"),
-             EqInlineKeyboardButton("▷", callback_data="{}_prev({})".format(prefix, modulo_page)))]
-
-    else:
-        pairs +=[[EqInlineKeyboardButton("Nexus", url="https://t.me/NexusXSupport")]]
-
+    if calc in [1, 2]:
+        pairs.append((modules[-1],))
     return pairs
 
 
-def send_to_list(bot: Bot,
-                 send_to: list,
-                 message: str,
-                 markdown=False,
-                 html=False) -> None:
+def send_to_list(
+    bot: Bot, send_to: list, message: str, markdown=False, html=False,
+) -> None:
     if html and markdown:
         raise Exception("Can only send with either markdown or HTML!")
     for user_id in set(send_to):
         try:
             if markdown:
-                bot.send_message(
-                    user_id, message, parse_mode=ParseMode.MARKDOWN)
+                bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN)
             elif html:
                 bot.send_message(user_id, message, parse_mode=ParseMode.HTML)
             else:
@@ -143,6 +126,21 @@ def build_keyboard_parser(bot, chat_id, buttons):
 
     return keyb
 
+
+def is_module_loaded(name):
+    return name not in NO_LOAD
+
+#function to mention username for chats https://t.me/username
+def mention_username(username: str, name: str) -> str:
+    """
+    Args:
+        username (:obj:`str`): The username of chat which you want to mention.
+        name (:obj:`str`): The name the mention is showing.
+    Returns:
+        :obj:`str`: The inline mention for the user as HTML.
+    """
+    return f'<a href="t.me/{username}">{escape(name)}</a>'
+
 def convert_gif(input):
     """Function to convert mp4 to webm(vp9)"""
 
@@ -179,5 +177,4 @@ def convert_gif(input):
             .run()
     )
 
-def is_module_loaded(name):
-    return name not in NO_LOAD
+    return converted_name
