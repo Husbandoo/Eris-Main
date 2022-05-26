077769f6af78f967f5c26cca4709c2f464f9d501
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2022-05-26 21:35:58 +0530

    Update __init__.py

--- NekoRobot/modules/__init__.py
+++ NekoRobot/modules/__init__.py
@@ -1,340 +1,162 @@
-import asyncio
-import logging
-import os
-import sys
-import json
-import asyncio
-import time
-import spamwatch
-import telegram.ext as tg
+"""
+MIT License
+Copyright (C) 2022-3033, AASFCYBERKING
+This file is part of @NekoXRobot (Telegram Bot)
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import requests
+import random
+from NekoRobot import dispatcher
+from NekoRobot.modules.disable import MessageHandler
+from telegram.ext import run_async, Filters
+from telegram import Message
 
-from inspect import getfullargspec
-from aiohttp import ClientSession
-from Python_ARQ import ARQ
-from telethon import TelegramClient
-from telethon.sessions import StringSession
-from telethon.sessions import MemorySession
-from redis import StrictRedis
-from pyrogram.types import Message
-from pyrogram import Client, errors
-from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid, ChannelInvalid
-from pyrogram.types import Chat, User
-from ptbcontrib.postgres_persistence import PostgresPersistence
-
-StartTime = time.time()
-
-def get_user_list(__init__, key):
-    with open("{}/NekoRobot/{}".format(os.getcwd(), __init__), "r") as json_file:
-        return json.load(json_file)[key]
-
-# enable logging
-FORMAT = "[NekoRobot] %(message)s"
-logging.basicConfig(
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-    format=FORMAT,
-    datefmt="[%X]",
+OWO = (
+    "*Neko pats {} on the head.",
+    "*gently rubs {}'s head*.",
+    "*Neko mofumofus {}'s head*",
+    "*Neko messes up {}'s head*",
+    "*Neko intensly rubs {}'s head*",
+    "*{}'s waifu pats their head*",
+    "*{}'s got free headpats*",
+    "No pats for {}!",
 )
-logging.getLogger("pyrogram").setLevel(logging.INFO)
-logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(logging.WARNING)
 
-LOGGER = logging.getLogger('[SiestaRobot]')
-LOGGER.info("Shikimori is starting. | Projected By Light. | Licensed under GPLv3.")
-LOGGER.info("Not affiliated to other anime or Villain in any way whatsoever.")
-LOGGER.info("Project maintained by: github.com/shiinobu (t.me/Light-Sensei)")
+K = (
+    "Vid"
+    "Text"
+    "Gif"
+) 
 
-# if version < 3.9, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 9:
-    LOGGER.error(
-        "You MUST have a python version of at least 3.9! Multiple features depend on this. Bot quitting."
+sleep_type = random.choice(K)
+
+
+def kiss(update, context):
+    url = "https://nekos.best/api/v2/kiss"
+    r = requests.get(url)
+    e = r.json()
+    kissme = e["results"][0]["url"]
+    msg = update.effective_message
+    msg.reply_video(kissme, caption="*Kisses u with all my love*~")
+
+
+
+def pat(update, context):
+    msg = update.effective_message
+    name = (
+        msg.reply_to_message.from_user.first_name
+        if msg.reply_to_message
+        else msg.from_user.first_name
     )
-    sys.exit(1)
+    url = "https://nekos.best/api/v2/pat"
+    r = requests.get(url)
+    e = r.json()
+    patme = e["results"][0]["url"]
+    msg.reply_video(patme, caption=random.choice(OWO).format(name))
 
-ENV = bool(os.environ.get("ENV", False))
 
-if ENV:
-    TOKEN = os.environ.get("TOKEN", None)
 
-    try:
-        OWNER_ID = int(os.environ.get("OWNER_ID", None))
-    except ValueError:
-        raise Exception("Your OWNER_ID env variable is not a valid integer.")
+def hug(update, context):
+    msg = update.effective_message
+    if msg.reply_to_message:
+       url = "https://nekos.best/api/v2/hug"
+       r = requests.get(url)
+       e = r.json()
+       hugme = e["results"][0]["url"]
+       msg = update.effective_message
+       name1 = msg.from_user.first_name
+       name2 = msg.reply_to_message.from_user.first_name
+       msg.reply_video(hugme, caption="*{} hugs {}*".format(name1, name2))
+    else:
+       url = "https://nekos.best/api/v2/hug"
+       r = requests.get(url)
+       e = r.json()
+       hugme = e["results"][0]["url"]
+       msg = update.effective_message
+       msg.reply_video(hugme, caption="*Hugs u with all my love*~")
+      
 
-    JOIN_LOGGER = os.environ.get("JOIN_LOGGER", None)
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+def slap(update, context):
+    msg = update.effective_message
+    if msg.reply_to_message:
+       url = "https://nekos.best/api/v2/slap"
+       r = requests.get(url)
+       e = r.json()
+       slapme = e["results"][0]["url"]
+       msg = update.effective_message
+       name1 = msg.from_user.first_name
+       name2 = msg.reply_to_message.from_user.first_name
+       msg.reply_video(slapme, caption="*{} slaps {}*".format(name1, name2))
+    else:
+       url = "https://nekos.best/api/v2/slap"
+       r = requests.get(url)
+       e = r.json()
+       slapme = e["results"][0]["url"]
+       msg = update.effective_message
+       msg.reply_video(slapme, caption="Here... Take this from me.")
 
-    try:
-        DRAGONS = {int(x) for x in os.environ.get("DRAGONS", "").split()}
-        DEV_USERS = {int(x) for x in os.environ.get("DEV_USERS", "").split()}
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
 
-    try:
-        DEMONS = {int(x) for x in os.environ.get("DEMONS", "").split()}
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
 
-    try:
-        WOLVES = {int(x) for x in os.environ.get("WOLVES", "").split()}
-    except ValueError: 
-        raise Exception("Your whitelisted users list does not contain valid integers.")
 
-    try:
-        TIGERS = {int(x) for x in os.environ.get("TIGERS", "").split()}
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
-
-    INFOPIC = bool(os.environ.get("INFOPIC", True))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
-    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
-    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
-    URL = os.environ.get("URL", "")  # Does not contain token
-    PORT = int(os.environ.get("PORT", 5000))
-    CERT_PATH = os.environ.get("CERT_PATH")
-    API_ID = os.environ.get("API_ID", None)
-    API_HASH = os.environ.get("API_HASH", None)
-    SESSION_STRING = os.environ.get("SESSION_STRING", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
-    DB_URL = os.environ.get("DATABASE_URL")
-    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
-    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-    REDIS_URL = os.environ.get("REDIS_URL", None) 
-    ARQ_API = os.environ.get("ARQ_API", None)
-    DONATION_LINK = os.environ.get("DONATION_LINK")
-    LOAD = os.environ.get("LOAD", "").split()
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-    TEMP_DOWNLOAD_LOC = os.environ.get("TEMP_DOWNLOAD_LOC" , None)
-    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
-    VIRUS_API_KEY = os.environ.get("VIRUS_API_KEY", None)
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
-    STRICT_GMUTE = bool(os.environ.get("STRICT_GMUTE", True))
-    WORKERS = int(os.environ.get("WORKERS", 8))
-    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
-    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
-    WALL_API = os.environ.get("WALL_API", None)
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
-    SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
-    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
-    LASTFM_API_KEY = os.environ.get("LASTFM_API_KEY", None)
-    CF_API_KEY = os.environ.get("CF_API_KEY", None)
-    WELCOME_DELAY_KICK_SEC = os.environ.get("WELCOME_DELAY_KICL_SEC", None)
-    BOT_ID = int(os.environ.get("BOT_ID", None))
-    ARQ_API_URL = "https://thearq.tech"
-    ARQ_API_KEY = ARQ_API
-    ERROR_LOGS = os.environ.get("ERROR_LOGS")
-
-    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
-
-    try:
-        BL_CHATS = {int(x) for x in os.environ.get("BL_CHATS", "").split()}
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
-
-else:
-    from NekoRobot.config import Development as Config
-
-    TOKEN = Config.TOKEN
-
-    try:
-        OWNER_ID = int(Config.OWNER_ID)
-    except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
-
-    JOIN_LOGGER = Config.JOIN_LOGGER
-    OWNER_USERNAME = Config.OWNER_USERNAME
-    ALLOW_CHATS = Config.ALLOW_CHATS
-    try:
-        DRAGONS = {int(x) for x in Config.DRAGONS or []}
-        DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
-
-    try:
-        DEMONS = {int(x) for x in Config.DEMONS or []}
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        WOLVES = {int(x) for x in Config.WOLVES or []}
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    try:
-        TIGERS = {int(x) for x in Config.TIGERS or []}
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
-
-    EVENT_LOGS = Config.EVENT_LOGS
-    WEBHOOK = Config.WEBHOOK
-    URL = Config.URL
-    PORT = Config.PORT
-    CERT_PATH = Config.CERT_PATH
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
-    ERROR_LOGS = Config.ERROR_LOGS
-    DB_URL = Config.SQLALCHEMY_DATABASE_URI
-    MONGO_DB_URI = Config.MONGO_DB_URI
-    ARQ_API = Config.ARQ_API_KEY
-    ARQ_API_URL = Config.ARQ_API_URL
-    DONATION_LINK = Config.DONATION_LINK
-    LOAD = Config.LOAD
-    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
-    OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
-    NO_LOAD = Config.NO_LOAD
-    HEROKU_API_KEY = Config.HEROKU_API_KEY
-    HEROKU_APP_NAME = Config.HEROKU_APP_NAME
-    DEL_CMDS = Config.DEL_CMDS
-    STRICT_GBAN = Config.STRICT_GBAN
-    WORKERS = Config.WORKERS
-    REM_BG_API_KEY = Config.REM_BG_API_KEY
-    BAN_STICKER = Config.BAN_STICKER
-    ALLOW_EXCL = Config.ALLOW_EXCL
-    CASH_API_KEY = Config.CASH_API_KEY
-    TIME_API_KEY = Config.TIME_API_KEY
-    WALL_API = Config.WALL_API
-    SUPPORT_CHAT = Config.SUPPORT_CHAT
-    SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
-    SPAMWATCH_API = Config.SPAMWATCH_API
-    SESSION_STRING = Config.SESSION_STRING
-    INFOPIC = Config.INFOPIC
-    BOT_USERNAME = Config.BOT_USERNAME
-    STRING_SESSION = Config.STRING_SESSION
-    LASTFM_API_KEY = Config.LASTFM_API_KEY
-    CF_API_KEY = Config.CF_API_KEY
-
-    try:
-        BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
-
-# If you forking dont remove this id, just add your id. LOL...
-
-DRAGONS.add(OWNER_ID)
-DRAGONS.add(2088106582)
-DRAGONS.add(945137470)
-DRAGONS.add(5099853374)
-DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(2088106582)
-DEV_USERS.add(945137470)
-DEV_USERS.add(5099853374)
-REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
-
-try:
-
-    REDIS.ping()
-
-    LOGGER.info("[SHASA]: Connecting To Shasa • Data Center • Mumbai • Redis Database")
-
-except BaseException:
-
-    raise Exception(
-        "[ERIS ERROR]: Your NAMI • Data Center • Mumbai • Redis Database Is Not Alive, Please Check Again."
-    )
-
-finally:
-
-    REDIS.ping()
-
-    LOGGER.info(
-        "[NAMI]: Connection To The Shasa • Data Center • Mumbai • Redis Database Established Successfully!"
+def cute(update, context):
+    msg = update.effective_message
+    name = msg.from_user.first_name
+    url = "https://nekos.best/api/v2/neko"
+    r = requests.get(url)
+    e = r.json()
+    cuteme = e["results"][0]["url"]
+    msg.reply_photo(
+        cuteme, caption="Thank UwU {}-Kun  *smiles and hides ^~^*".format(name)
     )
 
 
-
-if not SPAMWATCH_API:
-    sw = None
-    LOGGER.warning("SpamWatch API key missing! recheck your config")
-else:
-    try:
-        sw = spamwatch.Client(SPAMWATCH_API)
-    except:
-        sw = None
-        LOGGER.warning("Can't connect to SpamWatch!")
-
-from NekoRobot.modules.sql import SESSION
-
-defaults = tg.Defaults(run_async=True)
-updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient(MemorySession(), API_ID, API_HASH)
-dispatcher = updater.dispatcher
-print("[INFO]: INITIALIZING AIOHTTP SESSION")
-aiohttpsession = ClientSession()
-# ARQ Client
-print("[INFO]: INITIALIZING ARQ CLIENT")
-arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
-
-ubot2 = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
-try:
-    ubot2.start()
-except BaseException:
-    print("Userbot Error ! Have you added a STRING_SESSION in deploying??")
-    sys.exit(1)
-
-pbot = Client(
-    ":memory:",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=TOKEN,
-    workers=min(32, os.cpu_count() + 4),
-)
-apps = []
-apps.append(pbot)
-loop = asyncio.get_event_loop()
-
-async def get_entity(client, entity):
-    entity_client = client
-    if not isinstance(entity, Chat):
-        try:
-            entity = int(entity)
-        except ValueError:
-            pass
-        except TypeError:
-            entity = entity.id
-        try:
-            entity = await client.get_chat(entity)
-        except (PeerIdInvalid, ChannelInvalid):
-            for kp in apps:
-                if kp != client:
-                    try:
-                        entity = await kp.get_chat(entity)
-                    except (PeerIdInvalid, ChannelInvalid):
-                        pass
-                    else:
-                        entity_client = kp
-                        break
-            else:
-                entity = await kp.get_chat(entity)
-                entity_client = kp
-    return entity, entity_client
+def sleep(update, context):
+    if sleep_type == "Text":
+        msg = update.effective_message           
+        msg.reply_text(
+           ". . . (∪｡∪)｡｡｡zzzZZ"
+       )
+    if sleep_type == "Vid":      
+        msg = update.effective_message
+        bed = "https://telegra.ph/file/f0fb71c72e059de34b565.mp4"
+        msg.reply_video(
+            bed
+       )
+    if sleep_type == "Gif":
+       msg = update.effective_message
+       url = "https://nekos.best/api/v2/sleep"
+       r = requests.get(url)
+       e = r.json()
+       sleepme = e["results"][0]["url"]
+       msg.reply_video(
+           sleepme
+    )
 
 
-async def eor(msg: Message, **kwargs):
-    func = msg.edit_text if msg.from_user.is_self else msg.reply
-    spec = getfullargspec(func.__wrapped__).args
-    return await func(**{k: v for k, v in kwargs.items() if k in spec})
+KISS_HANDLER = MessageHandler(Filters.regex("(?i)Neko kiss"), kiss, run_async=True)
+PAT_HANDLER = MessageHandler(Filters.regex("(?i)Neko pat"), pat, run_async=True)
+HUG_HANDLER = MessageHandler(Filters.regex("(?i)Neko hug"), hug, run_async=True)
+SLAP_HANDLER = MessageHandler(Filters.regex("(?i)Neko slap"), slap, run_async=True)
+CUTE_HANDLER = MessageHandler(Filters.regex("(?i)Neko cute"), cute, run_async=True)
+SLEEP_HANDLER = MessageHandler(Filters.regex('^(Neko sleep|sleep)$'), sleep, run_async=True)
 
-
-DRAGONS = list(DRAGONS) + list(DEV_USERS)
-DEV_USERS = list(DEV_USERS)
-WOLVES = list(WOLVES)
-DEMONS = list(DEMONS)
-TIGERS = list(TIGERS)
-
-# Load at end to ensure all prev variables have been set
-from NekoRobot.modules.helper_funcs.handlers import (
-    CustomCommandHandler,
-    CustomMessageHandler,
-    CustomRegexHandler,
-)
-
-# make sure the regex handler can take extra kwargs
-tg.RegexHandler = CustomRegexHandler
-tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
+dispatcher.add_handler(KISS_HANDLER)
+dispatcher.add_handler(PAT_HANDLER)
+dispatcher.add_handler(HUG_HANDLER)
+dispatcher.add_handler(SLAP_HANDLER)
+dispatcher.add_handler(CUTE_HANDLER)
+dispatcher.add_handler(SLEEP_HANDLER)
