81a5915c0f38764906b25d7c7f573543fb572ed7
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-06-01 01:32:10 +0530

    Update misc.py

--- NekoRobot/modules/helper_funcs/misc.py
+++ NekoRobot/modules/helper_funcs/misc.py
@@ -144,6 +144,53 @@ def build_keyboard_parser(bot, chat_id, buttons):
     return keyb
 
 def convert_gif(input):
+    def mention_username(username: str, name: str) -> str:
+    """
+    Args:
+        username (:obj:str): The username of chat which you want to mention.
+        name (:obj:str): The name the mention is showing.
+    Returns:
+        :obj:str: The inline mention for the user as HTML.
+    """
+    return f'<a href="t.me/{username}">{escape(name)}</a>'
+
+def convert_gif(input):
+    """Function to convert mp4 to webm(vp9)"""
+
+    vid = cv2.VideoCapture(input)
+    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+    #check height and width to scale
+    if width > height:
+        width = 512
+        height = -1
+    elif height > width:
+        height = 512
+        width = -1
+    elif width == height:
+        width = 512
+        height = 512
+
+
+    converted_name = 'kangsticker.webm'
+
+    (
+        ffmpeg
+            .input(input)
+            .filter('fps', fps=30, round="up")
+            .filter('scale', width=width, height=height)
+            .trim(start="00:00:00", end="00:00:03", duration="3")
+            .output(converted_name, vcodec="libvpx-vp9", 
+                        **{
+                            #'vf': 'scale=512:-1',
+                            'crf': '30'
+                            })
+            .overwrite_output()
+            .run()
+    )
+
+    return converted_name
     """Function to convert mp4 to webm(vp9)"""
 
     vid = cv2.VideoCapture(input)
@@ -181,3 +228,5 @@ def convert_gif(input):
 
 def is_module_loaded(name):
     return name not in NO_LOAD
+
+def is_module_loaded(name): return name not in NO_LOAD
